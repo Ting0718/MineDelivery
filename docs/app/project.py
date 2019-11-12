aa3eddc9c619107c467979505b37196ebd5f99c9
Christine <christinehxr@gmail.com>
2019-11-11 17:33:57 -0800

    world setup

---
+++ docs/app/project.py
@@ -0,0 +1,94 @@
+import MalmoPython
+import json
+import logging
+import os
+import random
+import sys
+import time
+import numpy
+import matplotlib.pyplot as plt
+from collections import defaultdict, deque
+from priority_dict import priorityDictionary as PQ
+
+def load_grid(world_state):
+    """
+    Used the agent observation API to get a 21 X 21 grid box around the agent (the agent is in the middle).
+
+    Args
+        world_state:    <object>    current agent world state
+
+    Returns
+        grid:   <list>  the world grid blocks represented as a list of blocks (see Tutorial.pdf)
+    """
+    while world_state.is_mission_running:
+        #sys.stdout.write(".")
+        time.sleep(0.1)
+        world_state = agent_host.getWorldState()
+        if len(world_state.errors) > 0:
+            raise AssertionError('Could not load grid.')
+
+        if world_state.number_of_observations_since_last_state > 0:
+            msg = world_state.observations[-1].text
+            observations = json.loads(msg)
+            grid = observations.get(u'floorAll', 0)
+            break
+    return grid
+
+agent_host = MalmoPython.AgentHost()
+try:
+    agent_host.parse(sys.argv)
+except RuntimeError as e:
+    print('ERROR:', e)
+    print(agent_host.getUsage())
+    exit(1)
+if agent_host.receivedArgument("help"):
+    print(agent_host.getUsage())
+    exit(0)
+
+if agent_host.receivedArgument("test"):
+    num_repeats = 1
+else:
+    num_repeats = 10
+
+for i in range(num_repeats):
+    # size = int(6 + 0.5*i)
+    # print("Size of maze:", size)
+    # my_mission = MalmoPython.MissionSpec(GetMissionXML("0", 0.4 + float(i/20.0), size), True)
+    mission_file = './world.xml'
+    with open(mission_file, 'r') as f:
+        print("Loading mission from %s" % mission_file)
+        mission_xml = f.read()
+        my_mission = MalmoPython.MissionSpec(mission_xml, True)
+    my_mission_record = MalmoPython.MissionRecordSpec()
+    my_mission.requestVideo(800, 500)
+    my_mission.setViewpoint(1)
+    # Attempt to start a mission:
+    max_retries = 3
+    my_clients = MalmoPython.ClientPool()
+    my_clients.add(MalmoPython.ClientInfo('127.0.0.1', 10000)) # add Minecraft machines here as available
+
+    for retry in range(max_retries):
+        try:
+            agent_host.startMission(my_mission, my_clients, my_mission_record, 0, "%s-%d" % ('Moshe', i))
+            break
+        except RuntimeError as e:
+            if retry == max_retries - 1:
+                print("Error starting mission", (i+1), ":", e)
+                exit(1)
+            else:
+                time.sleep(2)
+
+    # Loop until mission starts:
+    print("Waiting for the mission", (i+1), "to start ",)
+    world_state = agent_host.getWorldState()
+    while not world_state.has_mission_begun:
+        time.sleep(0.1)
+        world_state = agent_host.getWorldState()
+        for error in world_state.errors:
+            print("Error:", error.text)
+
+    print()
+    print("Mission", (i+1), "running.")
+
+    grid = load_grid(world_state)
+    print('........', grid)
